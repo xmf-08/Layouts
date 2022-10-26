bf2f2bbab0260b28ecbca78a5339e8b12729f1dd
clay <muhammadyusuf020208@gmail.com>
2022-10-26 18:31:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/hm3/hm7.kt
@@ -0,0 +1,11 @@
+package com.example.hm3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hm7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hm7)
+    }
+}
---
+++ app/src/main/java/com/example/hm3/homework1.kt
@@ -0,0 +1,11 @@
+package com.example.hm3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homework1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework1)
+    }
+}
